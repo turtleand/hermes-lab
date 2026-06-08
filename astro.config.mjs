ce0f8ead1b53d524f226739282a34bbba5801f81
TurtleandHermes <hermes@turtleand.com>
2026-06-08 00:10:09 +0000

    docs: scaffold Turtleand Hermes Lab

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx(), sitemap()],
+  site: 'https://hermes.turtleand.com',
+});
